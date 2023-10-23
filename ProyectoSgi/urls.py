4c4551233e70f74bd9315de16d99689061799ce1
TALHIA VANESA PELAEZ RIOS <tvpelez@misena.edu.co>
2023-10-23 15:28:35 -0500

    Generar pdf y excel en elementos

--- ProyectoSgi/urls.py
+++ ProyectoSgi/urls.py
@@ -38,6 +38,8 @@ urlpatterns = [
     path('formElementos/', views.formElementos_view, name='formElementos_view'),
     path('listarElementos/', views.listar_elementos, name='listar_elementos'),
     path('eliminarElemento/<int:id>/', views.eliminarElemento, name='eliminarElemento'), #eliminar registro de la base de datos desde consultar elementos 
+    path('generar_pdf/', views.generar_pdf, name='generar_pdf'),
+    path('generar_excel/', views.generar_excel, name='generar_excel'),
 
 ]
 
